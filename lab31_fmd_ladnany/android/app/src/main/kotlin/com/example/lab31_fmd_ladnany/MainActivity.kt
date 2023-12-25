08a1b3d56d90823d929627458c584bbe81f09670
ilyasladnany <iladnany@gmail.com>
2023-12-25 22:42:19 +0100

    v1

---
+++ lab31_fmd_ladnany/android/app/src/main/kotlin/com/example/lab31_fmd_ladnany/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab31_fmd_ladnany
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ voice_assistant-master/lib/secrets.dart
@@ -0,0 +1,1 @@
+const openAIAPIKey = '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';
